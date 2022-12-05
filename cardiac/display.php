b69e5dd65483fabb536e5e755c03a762a4d0bf9c
xhuetter <xjhuetter@yahoo.com>
2022-12-04 23:59:48 -0500

    Bringing in db code

---
+++ cardiac/display.php
@@ -0,0 +1,33 @@
+<?php
+
+$hostname = 'localhost';
+$username = 'root';
+$password = 'PoPpOp2001?';
+$database = 'cardiacvr';
+
+try
+{
+	$dbh = new PDO('mysql:host='. $hostname .';dbname='. $database,
+         $username, $password);
+}
+catch(PDOException $e)
+{
+	echo '<h1>An error has occurred.</h1><pre>', $e->getMessage()
+            ,'</pre>';
+}
+
+$sth = $dbh->query('SELECT * FROM students');
+$sth->setFetchMode(PDO::FETCH_ASSOC);
+
+$result = $sth->fetchAll();
+
+if (count($result) > 0)
+{
+	foreach($result as $r)
+	{
+		echo $r['SID'], "\n _";
+		echo $r['StudentName'], "\n _";
+	}
+}
+
+?>
